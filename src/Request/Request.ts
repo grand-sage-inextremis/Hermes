7e85b1697121f53e95dd0fcdae8f39ca449d31c1
Grand Sage InExtremis <grand.sage.inextremis@gmail.com>
2025-05-03 13:20:07 +0200

    feat(Request): add the implementation of the Hms_Request class

--- src/Request/Request.ts
+++ src/Request/Request.ts
@@ -1,44 +1,140 @@
 import { Hms_RequestInstance, Hms_RequestClass } from './RequestInterfaces';
+import { INVALID_URL } from '../utils/Error';
+import { addTrailingSlashToPathname, removeTrailingSlashFromPathname } from '../utils/url';
 
 
 
 const Hms_Request: Hms_RequestClass = class HRequest implements Hms_RequestInstance
 {
 	public readonly url: URL;
+	private _pathnameBaseLength: number;
 	
 
 
 	constructor(url: URL)
 	{
 		this.url = url;
+		this.url.pathname = removeTrailingSlashFromPathname(this.url.pathname);
+
+		this._pathnameBaseLength = 0;
 	}
 
 
 
 	public static create(url: URL | string): Hms_Request
 	{
-		return new Hms_Request(new URL('http://media-inextremis.net/'));
+		let url_asURL: URL;
+
+		if (typeof url === 'string')
+		{
+			try {
+				url_asURL = new URL(url);
+			}
+			catch (err)
+			{
+				throw INVALID_URL(url);
+			}
+		}
+		else {
+			url_asURL = url;
+		}
+
+		return new Hms_Request(url_asURL);
+		
 	}
 
 
 
 	public get pathnameBase(): string
 	{
-		return '';
+		if (this._pathnameBaseLength === 0)
+		{
+			return '/';
+		}
+
+		return this.url.pathname.slice(0, this._pathnameBaseLength);
 	}
 
 
 
 	public get relativePathname(): string
 	{
-		return '';
+		if (this._pathnameBaseLength === this.url.pathname.length)
+		{
+			return '/';
+		}
+		
+		return this.url.pathname.slice(this._pathnameBaseLength);
 	}
 
 
 
 	public updateRelativePathname(pathnameList: Array<string>): string
 	{
-		return '';
+		let matchingPathname = '';
+
+		for (const pathname of pathnameList)
+		{
+			if (this.matchesBeginningOfRelativePathname(pathname))
+			{
+				if (pathname.length > matchingPathname.length)
+				{
+					matchingPathname = pathname;
+				}
+			}
+		}
+
+		if (matchingPathname !== '' && matchingPathname !== '/')
+		{
+			this._pathnameBaseLength += matchingPathname.length;
+		}
+
+		return matchingPathname;
+	}
+
+
+
+	/**
+	 * Returns true if `pathname` is a valid pathname and if it matches the beginning of
+	 * `this.relativePathname`.
+	 * 
+	 * Returns false otherwise.
+	 */
+	private matchesBeginningOfRelativePathname(pathname: string): boolean
+	{
+		let pathnameWithTrailingSlash: string;
+		let relativePathnameWithTrailingSlash: string;
+
+		if (pathname === '/' || this.relativePathname === '/')
+		{
+			return pathname === this.relativePathname;
+		}
+
+		// Let's assume that this.relativePathname === "/hermes/docs".
+		//
+		// This method must return false if pathname === "/herme".
+		// However, "/herme" matches the beginning of "/hermes/docs".
+		// So I need to add a trailing slash to `pathname`,
+		// because "/herme/" does not match the beginning of "/hermes/docs".
+		//
+		// Also, this method must return true if pathnameWithTrailingSlash === "/hermes/docs/".
+		// However, "/hermes/docs/" does not match the beginning of "/hermes/docs".
+		// So I need to add a trailing slash to `this.relativePathname`,
+		// because "/hermes/docs/" matches the beginning of "/hermes/docs/".
+		//
+		pathnameWithTrailingSlash = addTrailingSlashToPathname(pathname);
+		relativePathnameWithTrailingSlash = addTrailingSlashToPathname(this.relativePathname);
+
+		// This method must return false if `pathname` is not a valid pathname.
+		// `addTrailingSlashToPathname` already checks whether `pathname` is a valid pathname or not,
+		// so we use the following condition instead of calling `isValidPathname`.
+		//
+		if (pathnameWithTrailingSlash === '')
+		{
+			return false;
+		}
+
+		return relativePathnameWithTrailingSlash.startsWith(pathnameWithTrailingSlash);
 	}
 }
 
