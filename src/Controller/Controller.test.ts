187fe61fd51b8233301d105b12a4ebe960ae71c9
Grand Sage InExtremis <grand.sage.inextremis@gmail.com>
2025-05-06 18:12:44 +0200

    test(Controller): add tests for the Hms_Controller class

---
+++ src/Controller/Controller.test.ts
@@ -0,0 +1,38 @@
+import { vi, describe, it, expect } from 'vitest';
+
+import Hms_Controller from './Controller';
+import Hms_Request from '../Request/Request';
+import Hms_Response from '../Response/Response';
+
+
+
+describe("`Hms_Controller` class", function ()
+{
+
+
+
+describe("Hms_Controller.prototype.run(req, res)", function ()
+{
+	it("runs the function passed as parameter of `Hms_Controller.create(controllerFunction)`", function ()
+	{
+		const controllerFunction = function (req: Hms_Request, res: Hms_Response) { };
+		
+		const mockControllerFunction = vi.fn(controllerFunction);
+
+		let controller = Hms_Controller.create(mockControllerFunction);
+
+		let req = Hms_Request.create('http://media-inextremis.net/planets/venus');
+		let res = new Hms_Response();
+
+		controller.run(req, res);
+
+		
+		expect(mockControllerFunction.mock.calls).toHaveLength(1);
+		expect(mockControllerFunction.mock.calls[0][0]).toBe(req);
+		expect(mockControllerFunction.mock.calls[0][1]).toBe(res);
+	});
+});
+
+
+
+});  // ends describe("`Hms_Controller` class", ... );
