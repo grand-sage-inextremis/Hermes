0e16bdef021905b031bdc52a61990428d5945efa
Grand Sage InExtremis <grand.sage.inextremis@gmail.com>
2025-09-13 20:09:41 +0200

    fix(index): fix the TS4023 error
    I had started a project with '@inextremis/hermes' as dependency. When I typed:
    	const displayInbox = Hermes.createController(...);
    I got the following error:
    	Exported variable 'displayInbox' has or is using name 'Hms_ControllerInstance' from external module
    	"/home/pacman/Bureau/PACMAN-Fedora/Dev/playground/test/node_modules/@inextremis/hermes/dist/index"
    	but cannot be named.ts(4023)

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,44 @@
 import Hms_Request from './Request/Request';
 import Hms_Response from './Response/Response';
-import { Hms_ControllerLike } from './Controller/ControllerLike';
+import { type Hms_ControllerLike } from './Controller/ControllerLike';
 import Hms_Controller from './Controller/Controller';
 import Hms_Router from './Router/Router';
+
+
+
+export { default as Hms_Request } from './Request/Request';
+export { default as Hms_Response } from './Response/Response';
+export { default as Hms_Controller } from './Controller/Controller';
+export { default as Hms_Router } from './Router/Router';
+
+
+
+export { type Hms_RequestInstance } from './Request/RequestInterfaces';
+export { type Hms_ControllerLike } from './Controller/ControllerLike';
+export { type Hms_ControllerInstance } from './Controller/ControllerInterfaces';
+export { type Hms_RouterInstance } from './Router/RouterInterfaces';
 
 
 
 namespace Hermes
 {
 	export type Request = Hms_Request;
-	export const Request = Hms_Request;
-	export const createRequest: typeof Hms_Request.create = Hms_Request.create;
-	
 	export type Response = Hms_Response;
-	export const Response = Hms_Response;
-	export const createResponse = () => new Hms_Response();
-	
 	export interface ControllerLike extends Hms_ControllerLike {};
-	
 	export type Controller = Hms_Controller;
-	export const Controller = Hms_Controller;
-	export const createController: typeof Hms_Controller.create = Hms_Controller.create;
-	
 	export type Router = Hms_Router;
-	export const Router = Hms_Router;
-	export const createRouter: typeof Hms_Router.create = Hms_Router.create;
 
+	export const Request = Hms_Request;
+	export const Response = Hms_Response;
+	export const Controller = Hms_Controller;
+	export const Router = Hms_Router;
+	
+	export const createRequest: typeof Hms_Request.create = Hms_Request.create;
+	export const createResponse = () => new Hms_Response();
+	export const createRouter: typeof Hms_Router.create = Hms_Router.create;
+	export const createController: typeof Hms_Controller.create = Hms_Controller.create;
 }
 
 
 
-export {
-	Hermes as default,
-	Hms_Request,
-	Hms_Response,
-	Hms_ControllerLike,
-	Hms_Controller,
-	Hms_Router
-};
+export default Hermes;
